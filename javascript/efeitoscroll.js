e04a09bb2519a867e1404ec49bab265d01091059
Luiz Gustavo da Silva <38591825+Luiz4us8av6@users.noreply.github.com>
2022-06-07 17:40:40 -0300

    Commit add melhorias

--- javascript/efeitoscroll.js
+++ javascript/efeitoscroll.js
@@ -3,7 +3,7 @@ window.sr = ScrollReveal({   reset:true  });
 sr.reveal('.scroll', { duration:1000 });
 
 sr.reveal('.scroll2', { 
-    rotate:{ x:0,y:80,z:0},
+    transform:{ x:0,y:80,z:0},
     duration:2000
 });
 
